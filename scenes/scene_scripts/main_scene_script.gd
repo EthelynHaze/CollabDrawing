1ec3f9e63790fb176a6e8db6f1b450a98f34636d
EthelynHaze Desktop <ethelynhaze@gmail.com>
2025-07-22 15:02:33 +0700

    update: update home scene

--- scenes/scene_scripts/main_scene_script.gd
+++ scenes/scene_scripts/main_scene_script.gd
@@ -6,7 +6,7 @@ extends Control
 func _ready() -> void:
 	var tool_bar_scene = tool_bar_scene_load.instantiate()
 	var body_scene = body_scene_load.instantiate()
+	# var separator = VSeparator.new()
 	vbox_container.add_child(tool_bar_scene)	
-	print("Load toolbar scene")
+	# vbox_container.add_child(separator)
 	vbox_container.add_child(body_scene)
-	print("Load body scene")
