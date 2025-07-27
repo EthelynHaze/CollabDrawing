dba1288c96259f99d6dcf36b0ebd8f55f83fc60b
EthelynHaze <ethelynhaze@gmail.com>
2025-07-28 00:22:02 +0700

    update: 28072025

--- scenes/scene_scripts/dock_host.gd
+++ scenes/scene_scripts/dock_host.gd
@@ -1,13 +1,9 @@
 extends Control
 @onready var popup_menu = preload("res://scenes/sub_scenes/popup_menu_right_mouse.tscn")
 
-var popup_showing = false
 func _gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
-			_show_popup_menu_right_mouse(event.position)
+			print("Click right mouse button")
 
-func _show_popup_menu_right_mouse(mouse_position: Vector2) -> void:
-	var popup_menu_scene = popup_menu.instantiate()
-	self.add_child(popup_menu_scene)
-	popup_menu_scene.popup(Rect2(mouse_position, Vector2.ZERO))
+func _check_popup_
